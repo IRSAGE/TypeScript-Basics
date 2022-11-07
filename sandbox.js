b4d335abf65a9c2071c571a4e19ad31915502429
IRSAGE <sibo.egide98@gmail.com>
2022-11-07 22:19:18 +0200

    Working with arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,18 @@
-var character = "Hello";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ["hello"];
+names.push("hello2");
+// names.push(3); will not work
+var numbers = [10, 20, 30];
+numbers.push(25);
+//numbers[1] = "hi"; will not work
+var mixed = ["sibo", 4, "frontDev", 8]; //this works as an array of mixed both numbers and string
+mixed[0] = 3; // this wiil work you.
+//Objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
 };
-console.log(circ(3));
+ninja.age = 40; //this will work
+// ninja.age = ""; //this will not work
+// ninja.skills = ["fighting","running"]; //this will not work
--- sandbox.ts
+++ sandbox.ts
@@ -1,16 +1,25 @@
-const character = "Hello";
-console.log(character);
+// arrays
+let names = ["hello"];
 
-const inputs = document.querySelectorAll("input");
+names.push("hello2");
+// names.push(3); will not work
 
-console.log(inputs);
+let numbers = [10, 20, 30];
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+numbers.push(25);
+//numbers[1] = "hi"; will not work
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let mixed = ["sibo", 4, "frontDev", 8]; //this works as an array of mixed both numbers and string
+mixed[0] = 3; // this wiil work you.
+
+//Objects
+
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30, // they work as other variables you can not change the type of them after decralation.
 };
 
-console.log(circ(3));
+ninja.age = 40; //this will work
+// ninja.age = ""; //this will not work
+// ninja.skills = ["fighting","running"]; //this will not work
