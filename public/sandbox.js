5523044ef2a93a9196ac138a9c879f92d023fa74
IRSAGE <sibo.egide98@gmail.com>
2022-11-08 15:43:31 +0200

    Type Aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,18 +1,8 @@
 "use strict";
-//Functions
-let great;
-great = () => {
-    console.log("Hello");
+//Type Aliases
+const greet = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-great();
-const add = (a, b, c = 10) => {
-    //optional parameters should always puted on the end
-    console.log(a + b);
-    console.log(c);
-};
-add(5, 4); //this will work
-add(5, 4, 20); //as well this one also
-//specifying the return type of the function
-const minus = (a, b) => {
-    return b - a;
+const greet2 = (user) => {
+    console.log(`${user.name} with id  ${user.uid}`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,21 +1,12 @@
-//Functions
+//Type Aliases
 
-let great: Function;
-great = () => {
-  console.log("Hello");
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
+
+const greet = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
 };
 
-great();
-const add = (a: number, b: number, c: number | string = 10) => {
-  //optional parameters should always puted on the end
-  console.log(a + b);
-  console.log(c);
-};
-
-add(5, 4); //this will work
-add(5, 4, 20); //as well this one also
-
-//specifying the return type of the function
-const minus = (a: number, b: number): number => {
-  return b - a;
+const greet2 = (user: objWithName) => {
+  console.log(`${user.name} with id  ${user.uid}`);
 };
