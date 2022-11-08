dbabd5081f400836a5a3c04f3e16d6709141848b
IRSAGE <sibo.egide98@gmail.com>
2022-11-08 15:33:02 +0200

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Test");
+//Functions
+let great;
+great = () => {
+    console.log("Hello");
+};
+great();
+const add = (a, b, c = 10) => {
+    //optional parameters should always puted on the end
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 4); //this will work
+add(5, 4, 20); //as well this one also
+//specifying the return type of the function
+const minus = (a, b) => {
+    return b - a;
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log("Test");
+//Functions
+
+let great: Function;
+great = () => {
+  console.log("Hello");
+};
+
+great();
+const add = (a: number, b: number, c: number | string = 10) => {
+  //optional parameters should always puted on the end
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 4); //this will work
+add(5, 4, 20); //as well this one also
+
+//specifying the return type of the function
+const minus = (a: number, b: number): number => {
+  return b - a;
+};
