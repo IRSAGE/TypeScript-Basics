4cfda75b0e449af700499d362e8f42f50ebed602
IRSAGE <sibo.egide98@gmail.com>
2022-11-09 09:40:00 +0200

    Rendering Html Tempelate

--- public/app.js
+++ public/app.js
@@ -1,37 +1,6 @@
-//Class
-//class Initialization
-//all class properties are public by default
-//Public :means the property can be access anywhere
-//Private :means the property can only be access inside the class
-//readonly :means the property can be read outside and inside the class its self but we can't change it
 import { Invoices } from "./classes/invoice.js";
+import { ListTempelate } from "./classes/ListTempelate.js";
 import { Payment } from "./classes/Payment.js";
-//the Above declaration is same as this one below
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${amount} for ${details}`;
-    }
-}
-//class instancite an object from the class
-const invOne = new Invoices("me", "loan", 300);
-let invoices = []; //this will only allow objects created from the class Invoices
-invoices.push(invOne);
-console.log(invoices);
-const me = {
-    name: "Me",
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        return amount;
-    },
-};
 //const form = document.querySelector("form")!;
 const form = document.querySelector(".new-item-form");
 //Inputs
@@ -39,6 +8,9 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+//List Template Instance
+const ul = document.querySelector("ul");
+const list = new ListTempelate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -48,5 +20,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, Number(amount.value));
     }
-    console.log(doc);
+    list.render(doc, type.value, "start");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,7 @@
-//Class
-
-//class Initialization
-
-//all class properties are public by default
-
-//Public :means the property can be access anywhere
-//Private :means the property can only be access inside the class
-//readonly :means the property can be read outside and inside the class its self but we can't change it
 import { Invoices } from "./classes/invoice.js";
+import { ListTempelate } from "./classes/ListTempelate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-//the Above declaration is same as this one below
-class Invoice {
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes £${amount} for ${details}`;
-  }
-}
-//class instancite an object from the class
-const invOne = new Invoices("me", "loan", 300);
-
-let invoices: Invoices[] = []; //this will only allow objects created from the class Invoices
-
-invoices.push(invOne);
-console.log(invoices);
-
-//Interfaces
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
-const me: IsPerson = {
-  name: "Me",
-  age: 30,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    return amount;
-  },
-};
 
 //const form = document.querySelector("form")!;
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
@@ -57,6 +12,10 @@ const tofrom = document.querySelector("#tofrom") as HTMLSelectElement;
 const details = document.querySelector("#details") as HTMLSelectElement;
 const amount = document.querySelector("#amount") as HTMLSelectElement;
 
+//List Template Instance
+const ul = document.querySelector("ul")!;
+const list = new ListTempelate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
@@ -66,5 +25,5 @@ form.addEventListener("submit", (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, Number(amount.value));
   }
-  console.log(doc);
+  list.render(doc, type.value, "start");
 });
